2dfee6ca116208f8b7d7ccd29e7030205782e411
ShomaFujii <soccer.11.sho-ma@i.softbank.jp>
2021-05-02 18:53:01 +0900

    commit

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,40 +5,40 @@ class RoomsController < ApplicationController
 
   def index
     @rooms = Room.order("created_at DESC")
- end
+  end
 
- def new
-   @room = Room.new
- end
+  def new
+    @room = Room.new
+  end
 
- def create
+  def create
    @room = Room.new(room_params)
    if @room.save
      redirect_to root_path
    else
      render :new
    end
- end
-
- def show 
-  @comment = Comment.new
-  @comments = @room.comments.includes(:user)
-  @review = Review.new
- end
-
- def edit
-
- end
-
- def destroy
-  unless @room.user_id == current_user.id
-    redirect_to action: :index
   end
-  @room.destroy
-  redirect_to root_path
- end
+ 
+  def show 
+   @comment = Comment.new
+   @comments = @room.comments.includes(:user)
+   @review = Review.new
+  end
 
- def update
+  def edit
+
+  end
+
+  def destroy
+   unless @room.user_id == current_user.id
+    redirect_to action: :index
+   end
+    @room.destroy
+    redirect_to root_path
+  end
+
+  def update
 
   if @room.update(room_params)
    redirect_to root_path
@@ -47,26 +47,26 @@ class RoomsController < ApplicationController
    render :edit 
   end
 
- end
-
-def search
-  @rooms = Room.search(params[:keyword])
-end
-
- private
-
- def set_remove
-  unless @room.user_id == current_user.id
-    redirect_to action: :index
   end
-end
 
- def room_params
-  params.require(:room).permit(:rent,:image,:address,:station,:date,:phone_number,:mail,:air_conditioner_id,:kitchen_id, :toilet_id,:bathroom_id,:pet_id,:discribe,:deposit,:price).merge(user_id: current_user.id)
- end
+  def search
+   @rooms = Room.search(params[:keyword])
+  end
 
- def set_item
-  @room = Room.find(params[:id])
-end
+  private
+
+  def set_remove
+   unless @room.user_id == current_user.id
+    redirect_to action: :index
+   end
+  end
+
+  def room_params
+   params.require(:room).permit(:rent,:image,:address,:station,:date,:phone_number,:mail,:air_conditioner_id,:kitchen_id, :toilet_id,:bathroom_id,:pet_id,:discribe,:deposit,:price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+   @room = Room.find(params[:id])
+  end
 
 end
